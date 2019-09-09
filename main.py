7257df46e2d87bac27a95bab042dd7c740ce9276
Kendall Jackson <kendallkj00@gmail.com>
2019-09-09 11:52:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import firebase_admin
 import google.cloud
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("./ServiceAccountKey.json")
+cred = credentials.Certificate("Key/ServiceAccountKey.json")
 app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
